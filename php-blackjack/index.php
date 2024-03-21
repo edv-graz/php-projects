1a60dbc9bd3720651e2ab7da18b75d97e03c9f99
D. Minkov <76888305+isabella-projects@users.noreply.github.com>
2024-03-21 01:11:49 +0100

    Code refactoring and readability improvements
    Readability improvements on function getRandomCard.
    Refactoring function dealCards and removing the global variable, which is considered BAD practice. With this improvement we pass the card deck array as an argument to the function dealCard and the init function as well.

--- php-blackjack/index.php
+++ php-blackjack/index.php
@@ -6,31 +6,34 @@ $cardDeck = [
 	[2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, [1, 11]],
 	[2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, [1, 11]]
 ];
-function getRandomCard(array &$cards): array
-{
-	$random_num = rand(0, 3);
+function getRandomCard(array &$cards): array {
+    $randomStackIndex = array_rand($cards);
+    $randomStack = &$cards[$randomStackIndex];
+    
+    $randomCardIndex = array_rand($randomStack);
+    $randomCard = $randomStack[$randomCardIndex];
 
-	return array_splice($cards[$random_num], rand(0, (count($cards[$random_num]) - 1)), 1);
+    if (is_array($randomCard)) {
+        array_splice($randomStack, $randomCardIndex, 1);
+        return $randomCard;
+    }
+
+    array_splice($randomStack, $randomCardIndex, 1);
+
+    return [$randomCard];
 }
 
-function dealCards(): array
-{
-	global $cardDeck;
-	$check1 = getRandomCard($cardDeck);
-	$check2 = getRandomCard($cardDeck);
-	if (is_array($check1[0])) {
-		$check1[0] = $check1[0][rand(0, 1)];
-	}
-	if (is_array($check2[0])) {
-		$check2[0] = $check2[0][rand(0, 1)];
-	}
-
-	[$cards[]] = $check1;
-	[$cards[]] = $check2;
-
-	return $cards;
+function dealCards(array &$cardDeck, int $numCards = 2): array {
+    $cards = [];
+    for ($i = 0; $i < $numCards; $i++) {
+        $check = getRandomCard($cardDeck);
+        if (is_array($check[0])) {
+            $check[0] = $check[0][rand(0, 1)];
+        }
+        $cards[] = $check[0];
+    }
+    return $cards;
 }
-
 
 function whoWon($p_cards, $c_cards): int
 {
@@ -42,14 +45,14 @@ function whoWon($p_cards, $c_cards): int
 	return($c_sum > $p_sum) ? 1 : -1;
 }
 
-function game(): string
+function game(&$cardDeck): string
 {
 	$round_counter = 0;
 	$player_points = 0;
 	$dealer_points = 0;
 	while ($round_counter < 5) {
-		$player_cards = dealCards();
-		$dealer_cards = dealCards();
+		$player_cards = dealCards($cardDeck);
+		$dealer_cards = dealCards($cardDeck);
 		$who_won = whoWon($player_cards, $dealer_cards);
 		($who_won > 0) ? $player_points++ : $dealer_points++;
 		echo "Player: $player_points vs. Dealer: $dealer_points";
@@ -61,5 +64,5 @@ function game(): string
 	return($player_points > $dealer_points) ? 'Spieler:in hat gewonnen' : 'Dealer:in hat gewonnen';
 }
 
-echo game();
+echo game($cardDeck);
 
